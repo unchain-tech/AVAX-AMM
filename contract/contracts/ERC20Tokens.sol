22f3a50a83f6cf93d6f42f9c1a71cb222f68ac18
ryojiroakiyama <ryojiro25@gmail.com>
2022-09-26 23:37:33 +0900

    comment

--- contract/contracts/ERC20Tokens.sol
+++ contract/contracts/ERC20Tokens.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// ehterで表すのは分かりづらくなるのでやめる
+// ehterで表すのは分かりづらくなるのでやめる, 桁をconstで用意する
 contract USDCToken is ERC20 {
     constructor() ERC20("USDC Token", "USDC") {
         _mint(msg.sender, 10000 ether);
