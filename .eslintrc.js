e4d20d082c642d304a6bcbed0fdc048ee4c52269
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-04-16 13:18:32 +0900

    feat: add formatter and linter

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["standard", "prettier", "plugin:node/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "import/no-duplicates": ["warn"],
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
+    ],
+    "node/no-unpublished-require": ["warn"],
+    "node/no-unpublished-import": ["warn"],
+    "node/no-missing-import": ["warn"],
+    "no-lone-blocks": ["off"],
+    "no-unused-vars": ["warn"],
+  },
+  settings: {
+    node: { tryExtensions: [".js", ".json", ".node", ".ts", ".d.ts"] },
+  },
+};
